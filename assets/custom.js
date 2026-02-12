117b3f2af15aa677bca94f360ad926ad771bd335
Chintan <voramanthan662@gmail.com>
2026-02-12 21:25:49 +0530

    Update

--- assets/custom.js
+++ assets/custom.js
@@ -13,4 +13,34 @@ $(document).ready(function(){
           $(this).addClass('active');
       }
   });
+
+  // Drag to scroll for product offer list
+  const slider = document.querySelector('.product-info__offer-list');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  if(slider) {
+    slider.addEventListener('mousedown', (e) => {
+      isDown = true;
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mouseup', () => {
+      isDown = false;
+      slider.classList.remove('active');
+    });
+    slider.addEventListener('mousemove', (e) => {
+      if(!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 2; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  }
 });
